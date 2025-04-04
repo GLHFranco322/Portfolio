cd647aae82d057dcfceb0dd0e6706ecdb07b9803
Franco Spiazzi <francospiazzi96@gmail.com>
2025-04-03 21:23:28 -0300

    agregados componentes

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,11 +1,34 @@
-import './Hamburger.css'
+import { useState } from 'react';
+import './Hamburger.css';
 
 export default function Hamburger() {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleMenu = () => {
+        setIsActive(!isActive);
+    };
+
     return (
-            <nav className="hamburger" id="hamburger">
-                <span></span>
-                <span></span>
-                <span></span>
+        <>
+            <nav className={`off-screen-menu ${isActive ? 'active' : ''}`}>
+                <ul className="off-screen-menu__list">
+                    <li><i><img src="/public/icons/user.png" alt="" /></i></li>
+                    <li><i><img src="/public/icons/sketch.png" alt="" /></i></li>
+                    <li><i><img src="/public/icons/whatsapp.png" alt="" /></i></li>
+                </ul>
             </nav>
-    )
+
+            <nav id="nav_menu">
+                <div
+                    className={`hamburger ${isActive ? 'active' : ''}`}
+                    id="hamburger"
+                    onClick={toggleMenu}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </nav>
+        </>
+    );
 }
