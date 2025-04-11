83d0e0de413b6d77466b1b11bb8503709b10ae37
GLHFranco322 <francospiazzi96@gmail.com>
2025-04-10 21:27:25 -0300

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import {Header} from './header'
 import { Footer } from './footer'
-import { SectionOne } from './sectionOne'
+import { SectionOne } from './SectionOne'
 import { SectionTwo } from './sectionTwo'
 import { SectionThree } from './sectionThree'
 import { Slider } from './components/slider'
